07adb34b8706e98dccb6ab9a1a7c8e7ed785cd93
YopYar <ypylaev@inbox.ru>
2017-03-27 16:50:50 +0500

    !

--- Yaroslav/20 olimp/O_019.pas
+++ Yaroslav/20 olimp/O_019.pas
@@ -1,5 +1,5 @@
 Program nO_019;
-var n,k,i,s: integer;
+var n,k,i,s,c: integer;
 function NOD(a,b: integer): integer;
   var c: integer;
   begin
@@ -9,19 +9,21 @@ function NOD(a,b: integer): integer;
     result:=a;
   end;
 procedure divisors(l_ch,l_k,l_s: integer);
-  var l_i,c: integer;
+  var l_i: integer;
   begin
     if l_k<k then
-      begin
-        for l_i:=l_ch+1 to n do
-          if (n mod l_i)=0 then
-            if NOD(l_ch,l_i)=1 then
-              if l_s*l_i<=n then divisors(l_i,l_k+1,l_s*l_i)
-      end
-    else s:=s+1;
+      for l_i:=l_ch+1 to n do
+        if (n mod l_i)=0 then
+          if NOD(l_ch,l_i)=1 then
+            if l_s*l_i<=n then 
+              if l_k+1=k then s:=s+1
+              else divisors(l_i,l_k+1,l_s*l_i);
   end;
 begin
   readln(n,k);
-  for i:=1 to n do divisors(i,1,i);
+  for i:=1 to n do 
+    begin
+      if (n mod i)=0 then divisors(i,1,i);
+    end;
   write(s);
 end.
